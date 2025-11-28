39c5b5dc5e99eb68595f720ba5813ac831597193
Isaac Rodriguez <isaac_024@hotmail.com>
2025-11-28 17:50:15 -0600

    FirstSteps add commands-input

--- FirstSteps.sh
+++ FirstSteps.sh
@@ -1,4 +1,14 @@
 #=======================================================================
 #                         ENUMERATION 
 #=======================================================================
+# User input for target ip
+read -p "Enter target IP: " target_ip
+#       Verify the connectivity & availability of the target ICMP
+ping "$target_ip"
+#       Scan of the target
+nmap -p- -sV "$target_ip"
+
+        #  nmap: Scan 
+        #  -p- : Full range of ports
+        #  -sV : Detect versions of services
 
